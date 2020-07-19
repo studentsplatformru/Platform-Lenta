19e3d94170e781b7e5901622f8e2cd35fdc9eb2f
Krylov Sergey <kssergeyru@gmail.com>
2020-07-19 19:29:57 +0300

    Add Swagger

--- endpoint/src/main/java/ru/studentsplatform/newswire/NewswireApplication.java
+++ endpoint/src/main/java/ru/studentsplatform/newswire/NewswireApplication.java
@@ -2,8 +2,12 @@ package ru.studentsplatform.newswire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.studentsplatform.*"})
+@EntityScan(basePackages = {"ru.studentsplatform.entities.*"})
 public class NewswireApplication {
 
 	public static void main(String[] args) {

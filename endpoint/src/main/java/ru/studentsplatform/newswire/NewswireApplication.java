04e06fad78c607fc439151362fcf52075de520e7
Krylov Sergey <kssergeyru@gmail.com>
2020-07-19 16:34:46 +0300

    Initial commit

---
+++ endpoint/src/main/java/ru/studentsplatform/newswire/NewswireApplication.java
@@ -0,0 +1,13 @@
+package ru.studentsplatform.newswire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewswireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewswireApplication.class, args);
+	}
+
+}
